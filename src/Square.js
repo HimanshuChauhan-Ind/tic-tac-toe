c82b276f8737420d3ad026137816066a9a923d25
HimanshuChauhan <him97cha@gmail.com>
2021-06-18 02:37:56 +0530

    initial commit

---
+++ src/Square.js
@@ -0,0 +1,12 @@
+// Single entry to the block
+import './Square.css'
+
+function Square(props) {
+    return (
+      <button className="square" onClick={props.onClick}>
+        {props.value}
+      </button>
+    );
+}
+
+export default Square
